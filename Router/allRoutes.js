cc7120ec404c7b2326506ca617bc80ee092cfca5
Bodygreg <bodygreg@yahoo.fr>
2026-01-02 20:50:47 +0100

    Creation pages authentification

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/carte", "La carte", "/pages/carte.html"),
+    new Route("/signin", "connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "inscription", "/pages/auth/signup.html"),
+    new Route("/account", "compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "changement mot de passe", "/pages/auth/editPassword.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
